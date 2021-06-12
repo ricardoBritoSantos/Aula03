7858616176d1ed53c7f60fb5db18648affb3f920
Ricardo <ricardo_brito_santos@hotmail.com>
2021-06-12 11:10:05 -0300

    Implementado o operador Xor

--- src/OperadorXou.java
+++ src/OperadorXou.java
@@ -13,10 +13,13 @@ public class OperadorXou {
 		temperatura = leitor.nextDouble();
 		
 		//Aqui é preciso criar um if que verifique as duas condições para que o os ventiladores sejam ligados ou não
+		if(umidade > 70 ^ temperatura > 26) {
+			System.out.println("Ligando os ventiladores");
+		}else {
+			System.out.println("Por favor, contate um agrônomo");
+		}
 		
-		leitor.close();
-		
-
+		leitor.close();	
 	}
 
 }
